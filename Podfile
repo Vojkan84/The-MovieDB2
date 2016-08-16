dbd1e925ea4d07179d8caabeabc5092aac5c3747
Vojkan Spasic <spasic.vojkan@gmail.com>
2016-08-16 07:57:45 +0200

    pods

---
+++ Podfile
@@ -0,0 +1,12 @@
+
+  use_frameworks!
+
+
+  target 'The Movie DB2' do
+	platform :ios, '9.0'
+
+	pod 'Alamofire', '~> 3.4'
+
+
+
+end
---
+++ Pods/Target Support Files/Pods-The Movie DB2/Pods-The Movie DB2-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_The_Movie_DB2VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_The_Movie_DB2VersionString[];
+

476eaf40620aafbc15460d1b380d032b614ad199
Vojkan Spasic <spasic.vojkan@gmail.com>
2016-09-04 12:50:06 +0200

    Outlets

---
+++ The Movie DB2/ProfileController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileController.swift
+//  The Movie DB2
+//
+//  Created by Vojkan Spasic on 9/4/16.
+//  Copyright © 2016 Vojkan Spasic. All rights reserved.
+//
+
+import Cocoa
+
+class ProfileController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var LoginButton: UIButton!
+    
+}

e776314a799fe26f70757c784375a2ce37ca80ae
Vojkan Spasic <spasic.vojkan@gmail.com>
2016-09-08 13:19:27 +0200

    ComingSoonController

---
+++ The Movie DB2/ComingSoonMoviesController.swift
@@ -0,0 +1,14 @@
+//
+//  ComingSoonMoviesController.swift
+//  The Movie DB2
+//
+//  Created by Vojkan Spasic on 9/8/16.
+//  Copyright © 2016 Vojkan Spasic. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ComingSoonMoviesController:UIViewController{
+
+}

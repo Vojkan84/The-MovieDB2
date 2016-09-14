f9063d29f1f77f094923137275c0d4df4e48d9c9
Vojkan Spasic <spasic.vojkan@gmail.com>
2016-09-14 10:09:15 +0200

    MovieDetailController implements basic data source protocols and updates movie with credits,videos and album

--- The Movie DB2/MovieDetailController.swift
+++ The Movie DB2/MovieDetailController.swift
@@ -8,19 +8,70 @@
 
 import UIKit
 import AlamofireImage
+import RealmSwift
 
 class MovieDetailController:UIViewController{
     
-    var movie:Movie?
+    var movieId:Int?
+    var movie:Results<Movie>?{
+        didSet{
+            
+            let URL = movie?.first?.moviePosterUrl
+            self.photoView.af_setImageWithURL(URL!)
+        }
+    }
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var photoView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        let URL = movie?.moviePosterUrl
-        self.photoView.af_setImageWithURL(URL!)
         
-        
-    
+        MovieService.sharedInstace.fetchCreditsForMovie(movieId: movieId!) {[weak self] (credits, error) in
+            if let err = error{
+                print(err)
+            }
+            else{
+                
+                let realm = try! Realm()
+                let movie = realm.objects(Movie.self).filter("movieID = \(self!.movieId!) ")
+                MovieService.sharedInstace.fetchMovieAlbum(movieId:self!.movieId!){(album, error) in
+                    if let err = error{
+                        print(err)
+                    }else{
+                        MovieService.sharedInstace.fetchMovieVideos(movieID: self!.movieId!) {(videos, error) in
+                            if let err = error{
+                                print(err)
+                            }else{
+                                let movieToken = realm.addNotificationBlock({[weak self] (notification, realm) in
+                                    self?.movie = realm.objects(Movie.self).filter("movieID = \(self!.movieId!) ")
+                                    })
+                                try! realm.write{
+                                    realm.create(Movie.self, value:["movieId":self!.movieId!,"credits":credits!,"album":album!,"videos":videos!],update:true)
+                                }
+                                movieToken.stop()
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+}
+
+extension MovieDetailController:UITableViewDataSource{
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 6
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+ 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }

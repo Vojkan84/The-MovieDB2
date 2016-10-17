8fbe98dd6f9782d829d170f572bf4467e01add39
Vojkan Spasic <spasic.vojkan@gmail.com>
2016-10-17 16:34:28 +0200

    search bar implemented

--- The Movie DB2/SearchController.swift
+++ The Movie DB2/SearchController.swift
@@ -8,23 +8,71 @@
 
 import UIKit
 
-class SearchController:UIViewController,UITableViewDataSource{
+class SearchController:UIViewController,UITableViewDataSource,UISearchBarDelegate{
     
-    let searchController = UISearchController(searchResultsController: nil)
+   
+    var data = ["a","ab","abc","abcd","abcd","abcde","acdef","abcdefg"]
+    var numbers = ["1","2","3","4","5"]
+    var filteredData = [String]()
+    var searchBarActive:Bool = false
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      searchBar.delegate = self
+
         
+    }
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchBarActive = true
         
+    }
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchBarActive = false
+    }
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBarActive = false
+    }
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBarActive = false
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = data.filter{ sentence in
+            return sentence.lowercaseString.containsString(searchText.lowercaseString)
+        }
+        if filteredData.count == 0{
+            searchBarActive = false
+        }else{
+            searchBarActive = true
+        }
+        tableView.reloadData()
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if searchBarActive && searchBar.text != ""{
+            return filteredData.count
+        }
+        
+        return data.count
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("SearchControllerCell")
+        let text:String
+        if searchBarActive && searchBar.text != ""{
+            text = filteredData[indexPath.row]
+        }else{
+            text = data[indexPath.row]
+        }
+        cell?.textLabel?.text = text
+        return cell!
     }
 }
+

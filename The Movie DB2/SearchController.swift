0ffdb423b32c9a309a9a85470436d4b570ff9503
Vojkan Spasic <spasic.vojkan@gmail.com>
2016-10-17 14:29:00 +0200

    implemented tableView in SearchController

--- The Movie DB2/SearchController.swift
+++ The Movie DB2/SearchController.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
-class SearchController:UIViewController{
+class SearchController:UIViewController,UITableViewDataSource{
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
